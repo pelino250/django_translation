12ae51291277e7d371df115f5e91deaa3c1c8914
pelino250 <pmutanguha@alueducation.com>
2024-03-04 15:57:59 +0200

    add translations

--- core/models.py
+++ core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-
-# Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(_("title"), max_length=100)
+    description = models.TextField(_("description"))
+
+    class Meta:
+        verbose_name = _('Blog')
+        verbose_name_plural = _('Blogs')
 
     def __str__(self):
         return self.title
-
-
